dbaf4dba3c4080a9db888ac28c0f9eb8bad96369
Rishabh <rishabhlakhaiyar27@gmail.com>
2023-09-20 12:19:33 +0530

    hooks for challenge generated

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,17 @@
 import '@/styles/globals.css'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import {ThirdwebProvider,ChainId} from "@thirdweb-dev/react"
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
+  const desiredChainId=ChainId.Mumbai
   const queryClient=new QueryClient();
   return (
-    <QueryClientProvider client={queryClient}>
+    <ThirdwebProvider>
+      <QueryClientProvider client={queryClient}>
 
-      <Component {...pageProps} />
-    </QueryClientProvider>
+        <Component {...pageProps} />
+      </QueryClientProvider>
+    </ThirdwebProvider>  
   );
 }
